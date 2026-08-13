a999683e7b2d3e07b953d245263be90eae79f615
Nelli Skogman <nelli.skogman@gmail.com>
2026-08-13 16:29:26 +0200

    Add the Runtime struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,8 @@
-use std::process::ExitCode;
+mod effect;
+mod runtime;
 
+pub use effect::Effect;
+pub use runtime::run;
+
+// TODO: Move this.
 pub struct Ui;
-
-pub enum Effect {
-    None,
-}
-
-pub fn run<App, Message, Backend>(
-    initial_state: App,
-    view: fn(&App, &mut Ui),
-    update: fn(&mut App, Message) -> Effect,
-    backend: Backend,
-) -> ExitCode {
-    // TODO: Construct a runtime.
-    let _ = (initial_state, view, update);
-
-    // TODO: Pass the runtime to the backend.
-    let _ = backend;
-
-    ExitCode::SUCCESS
-}
---
+++ src/runtime.rs
@@ -0,0 +1,30 @@
+use crate::Effect;
+use crate::Ui;
+use std::process::ExitCode;
+
+pub struct Runtime<App, Message> {
+    #[expect(unused)]
+    app: App,
+    #[expect(unused)]
+    view: fn(&App, &mut Ui),
+    #[expect(unused)]
+    update: fn(&mut App, Message) -> Effect,
+}
+
+pub fn run<App, Message, Backend>(
+    initial_state: App,
+    view: fn(&App, &mut Ui),
+    update: fn(&mut App, Message) -> Effect,
+    backend: Backend,
+) -> ExitCode {
+    let runtime = Runtime {
+        app: initial_state,
+        view,
+        update,
+    };
+
+    // TODO: Pass the runtime to the backend.
+    let _ = (backend, runtime);
+
+    ExitCode::SUCCESS
+}
